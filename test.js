4dec5e49f338c3c47c893dc97bf43729d169c07f
Aaron Porter <aaron.porter920@gmail.com>
2017-07-03 16:05:03 -0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,6 @@
+import Plotly.relayout as refresh
+
+var update = {
+    'xaxis.autorange': true,   // updates the xaxis range
+    'yaxis.autorange': false     // updates the end of the yaxis range
+};
