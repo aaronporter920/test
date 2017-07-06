cfa9f3cd3ad4e790040bb47ed998dbaf9ffcfa05
Aaron Porter <aaron.porter920@gmail.com>
2017-07-06 13:30:03 -0500

    Add files via upload

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 setInterval(function(){ 
-document.querySelectorAll('[data-title="Autoscale"]');.click()
+document.querySelectorAll('[data-title="Autoscale"]').click();
 console.log("Sup")
 }, 10005);
 
