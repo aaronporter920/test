19b75ac70932b090dc8fab855c5a79479b9a776a
Aaron Porter <aaron.porter920@gmail.com>
2017-07-06 14:49:07 -0500

    Add files via upload

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ bttns = document.querySelectorAll('div.modebar-group a.modebar-btn[data-title="A
 console.log(bttns);
 var i;
 for (i = 0; i < bttns.length; i++){
-    console.log(x);
-    x.click();
+    console.log(bttns[i]);
+    bttns[i].click();
     
                 }
 console.log("Sup");
