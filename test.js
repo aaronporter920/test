2b4b449d563f57f3c76d023eff85cae28cbb4daa
Aaron Porter <aaron.porter920@gmail.com>
2017-07-06 14:15:56 -0500

    Add files via upload

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 setInterval(function(){ 
 bttns = document.querySelectorAll('div.modebar-group a.modebar-btn[data-title="Autoscale"]'));
 for (x in bttns){
+    console.log(x)
     x.click();
                 }
 console.log("Sup")
