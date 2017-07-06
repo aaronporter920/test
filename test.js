d0647fb719ca147d852203d6bb896f3e48413bd3
Aaron Porter <aaron.porter920@gmail.com>
2017-07-06 12:56:42 -0500

    Add files via upload

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
 setInterval(function(){ 
-$('data-title'="Autoscale").click()
+//$('data-title'="Autoscale").click()
+console.log("Sup")
 }, 5000);
 
 
